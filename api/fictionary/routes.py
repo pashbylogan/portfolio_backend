0e87ef91004b8464f3e82dd6fe9ea6cde3aabf81
Logan Pashby <pashbylogan@gmail.com>
2023-04-30 17:58:17 -0400

    might as well have in one file

--- api/fictionary/routes.py
+++ api/fictionary/routes.py
@@ -1,7 +1,26 @@
 from api.fictionary import bp
-from api.fictionary.fictionary_logic import authenticate_api_key, define
 from flask import Response, request
+import os, openai
 
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+def define(model, prompt, num_return=1):
+    models = openai.Model.list()
+    return models
+
+API_KEYS = {os.getenv('API_KEY')}
+def authenticate_api_key(func):
+    def wrapper(*args, **kwargs):
+        api_key = request.headers.get("API-KEY")
+        if api_key is None:
+            return "API Key is missing", 401
+
+        if api_key not in API_KEYS:
+            return "Invalid API Key", 401
+
+        return func(*args, **kwargs)
+
+    return wrapper
 
 @bp.route('/')
 def index():
